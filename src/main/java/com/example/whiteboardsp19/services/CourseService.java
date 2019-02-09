ff1efbe45d607ebd48fb1d792adc808cce47984c
ziqiang <ziqiang@ccs.neu.edu>
2019-02-09 01:55:00 -0500

    update deleteCourse

--- src/main/java/com/example/whiteboardsp19/services/CourseService.java
+++ src/main/java/com/example/whiteboardsp19/services/CourseService.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import com.example.whiteboardsp19.model.Course;
 import com.example.whiteboardsp19.model.Module;
 
@@ -34,7 +34,7 @@ public class CourseService {
 		courses.add(cs5610);
 	}
 	
-	@DeleteMapping("/api/course/{id}")
+	@DeleteMapping("/api/courses/{id}")
 	public List<Course> deleteCourse(
 			@PathVariable("id") int courseId) {
 		courses = courses.stream()
