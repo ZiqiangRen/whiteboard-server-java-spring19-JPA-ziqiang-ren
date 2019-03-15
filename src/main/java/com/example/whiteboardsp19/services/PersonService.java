a443e5eb8df9784beb8eecff8b494c0ebf27710a
ziqiang <ziqiang@ccs.neu.edu>
2019-03-15 17:08:07 -0400

    update1

--- src/main/java/com/example/whiteboardsp19/services/PersonService.java
+++ src/main/java/com/example/whiteboardsp19/services/PersonService.java
@@ -19,6 +19,7 @@ import com.example.whiteboardsp19.model.Person;
 import com.example.whiteboardsp19.repository.PersonRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials="true")
 public class PersonService {
 	
 	@Autowired
